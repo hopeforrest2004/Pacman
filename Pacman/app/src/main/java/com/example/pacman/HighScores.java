6c204383162988980a810b6138dc26f7807f83c1
Justin Gaspar <6335799+Critwhale@users.noreply.github.com>
2024-12-02 18:52:15 -0600

    Highscore Screen Added

--- Pacman/app/src/main/java/com/example/pacman/HighScores.java
+++ Pacman/app/src/main/java/com/example/pacman/HighScores.java
@@ -1,6 +1,8 @@
 package com.example.pacman;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HighScores extends AppCompatActivity {
+
+    private final TextView[] textViews = new TextView[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,82 @@ public class HighScores extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textViews[0] = (TextView) findViewById(R.id.text_highscore_0);
+        textViews[1] = (TextView) findViewById(R.id.text_highscore_1);
+        textViews[2] = (TextView) findViewById(R.id.text_highscore_2);
+        textViews[3] = (TextView) findViewById(R.id.text_highscore_3);
+        textViews[4] = (TextView) findViewById(R.id.text_highscore_4);
+        textViews[5] = (TextView) findViewById(R.id.text_highscore_5);
+        textViews[6] = (TextView) findViewById(R.id.text_highscore_6);
+        textViews[7] = (TextView) findViewById(R.id.text_highscore_7);
+        textViews[8] = (TextView) findViewById(R.id.text_highscore_8);
+        textViews[9] = (TextView) findViewById(R.id.text_highscore_9);
+
+        displayHighScores();
+
+        //updateHighScores("jim", 9999);
+    }
+
+    public void updateHighScores(String name, int score) {
+        SharedPreferences prefs = getSharedPreferences("HIGH_SCORES", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        HighScorePair[] highScores = new HighScorePair[10];
+
+        HighScorePair[] scores = getHighScores();
+
+        int pos = 10;
+        for(int i = 0; i < 10; i++) {
+            if ( score > scores[i].score) {
+                pos = i;
+                break;
+            }
+        }
+
+        // Not a highscore
+        if(pos == 10)
+            return;
+
+        for(int i = 10; i > pos; i--) {
+            editor.putInt("SCORES_" + i,  scores[i-1].score);
+            editor.putString("NAMES_" + i, scores[i-1].name);
+        }
+
+        editor.putInt("SCORES_" + pos, score);
+        editor.putString("NAMES_" + pos, name);
+        editor.apply();
+
+        displayHighScores();
+    }
+
+    private HighScorePair[] getHighScores() {
+        SharedPreferences prefs = getSharedPreferences("HIGH_SCORES", MODE_PRIVATE);
+        HighScorePair[] highScores = new HighScorePair[10];
+
+        for ( int i = 0; i < 10; i++ ) {
+            int score = prefs.getInt("SCORES_" + i, 0);
+            String name = prefs.getString("NAMES_" + i, "AAA");
+            highScores[i] = new HighScorePair(name, score);
+        }
+        return highScores;
+    }
+
+    private void displayHighScores() {
+        HighScorePair[] scores = getHighScores();
+
+        for ( int i = 0; i < 10; i++ ) {
+            String text = (i+1) + ". " + scores[i].score + " " + scores[i].name;
+            textViews[i].setText(text);
+        }
+    }
+
+    private static class HighScorePair {
+        public final String name;
+        public int score;
+
+        public HighScorePair(String name, int score) {
+            this.name = name;
+            this.score = score;
+        }
     }
 }
